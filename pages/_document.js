d1bb5f2d2aad8b7d16f087f0277b5d3a61713c8e
egrietqalbi <egrietqalbijati@gmail.com>
2022-07-06 12:55:25 +0700

    front4

---
+++ pages/_document.js
@@ -0,0 +1,38 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+    <div>
+  <meta charSet="utf-8" />
+  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+  <title>Restaurant One Page HTML5 Template</title>
+  <meta name="description" content />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  {/* CSS
+  ================================================ */}
+  {/* Owl Carousel */}
+  <link rel="stylesheet" href="css/owl.carousel.css" />
+  {/* bootstrap.min css */}
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  {/* Font-awesome.min css */}
+  <link rel="stylesheet" href="css/font-awesome.min.css" />
+  {/* Main Stylesheet */}
+  <link rel="stylesheet" href="css/animate.min.css" />
+  <link rel="stylesheet" href="css/main.css" />
+  {/* Responsive Stylesheet */}
+  <link rel="stylesheet" href="css/responsive.css" />
+</div>
+
+    
+ 
+      <body >
+        
+        <Main />
+        <NextScript />
+       {/*  */}
+
+      </body>
+    </Html>
+  )
+}
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+
+
+ export default function handler(req, res) {
+ const lokasi =path.join(__dirname+"../../../../../database");
+   const dataJson=fs.readFileSync(`${lokasi}/data.json`,{encording:"utf-8"});
+   res.status(200).json({ dataJson:JSON.parse(dataJson) })  ;
+ }
